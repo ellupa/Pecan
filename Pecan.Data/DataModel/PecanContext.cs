d8a6a5668b29cb2f095d427c3a584ddb9b5f4ae6
agus lupa <agusluparini@gmail.com>
2023-03-29 19:13:03 -0300

    Data Context Configuration Data connection

---
+++ Pecan.Data/DataModel/PecanContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pecan.Data.DataModel
+{ 
+    public class PecanContext : DbContext
+    {
+        public PecanContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseMySQL(@"Server=aws.connect.psdb.cloud;Database=smartstore;user=mssznfxvmkzidmqjqyzw;password=pscale_pw_oUkXJR3Yg7505H3FkRUYY1Go4nyyq9pKJDytqhumibM;SslMode=VerifyFull;");
+        }
+    }
+}
