f8b70872da2ab7c778f492442c78640ee6bd4958
Matias Lupo <matiezelupo@gmail.com>
2023-03-31 16:10:32 -0300

    PEC-5
    Created DataModel for Sale entity on Pecan.Data

---
+++ Pecan.Data/DataModel/SaleModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pecan.Data.DataModel
+{
+    public class SaleModel
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Id { get; set; }
+        public DateTime DateOfSale { get; set; }
+        public float Total { get; set; }
+    }
+}
