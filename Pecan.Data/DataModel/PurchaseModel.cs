ad9ef11b2970842d20d550ecbc559b5b704e228e
Matias Lupo <matiezelupo@gmail.com>
2023-03-31 16:09:49 -0300

    PEC-3
    Created DataModel for Purchase entity on Pecan.Data

---
+++ Pecan.Data/DataModel/PurchaseModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pecan.Data.DataModel
+{
+    public class PurchaseModel
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Id { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public int IdSupplier { get; set; }
+        public float Total { get; set; }
+    }
+}
